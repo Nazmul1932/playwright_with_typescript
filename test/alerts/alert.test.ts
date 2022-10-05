337a98b16dcd8a01e8b428709f9109d9ec4e5d2a
Nh <bsse1032@iit.du.ac.bd>
2022-10-05 12:09:13 +0600

    handling different types of alert

--- test/alerts/alert.test.ts
+++ test/alerts/alert.test.ts
@@ -24,22 +24,57 @@ describe('All type of alert testing', () => {
 
     })
 
-    test("testing prompt alert", async ()=>{
+    // test("testing prompt alert", async ()=>{
 
-        const element = await page.locator("xpath=//button[@id='prompt']")
+    //     const element = await page.locator("xpath=//button[@id='prompt']")
+
+    //     page.on("dialog", (dialog)=>{
+
+    //         console.log("message:" + dialog.message())
+    //         console.log("default value: " + dialog.defaultValue())
+    //         console.log("type:" + dialog.type())
+    //         dialog.accept("hello nazu")
+
+    //     })
+
+    //     await element?.click()
+
+    // })
+
+    // test('Accept the alert', async () => {
+
+    //     const element = await page.locator("xpath=//button[@id='accept']")
+
+    //     page.on('dialog', async (dialog) => {
+    //         console.log("message:" + dialog.message())
+    //         console.log("default value: " + dialog.defaultValue())
+    //         console.log("type:" + dialog.type())
+    //         dialog.accept()
+    //     })
+    //     await element?.click()
+    //   })
+
+    test('confirmation the alert', async () => {
+
+        const element = await page.locator("xpath=//button[@id='confirm']")
+
+        page.on('dialog', async (dialog) => {
+            expect(dialog.message()).toContain('Are you happy with LetCode?');
+            await dialog.accept();
+            
+        })
         await element?.click()
-
-    })
-
+      })
 
 
 
 
-    afterAll(async () => {
-        await page.close()
-        await context.close()
-        await browser.close()
-    });
+
+    // afterAll(async () => {
+    //     await page.close()
+    //     await context.close()
+    //     await browser.close()
+    // });
 
 
 })
